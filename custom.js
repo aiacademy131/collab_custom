3cb16e3cbc2b8a1c138602a40b48e0a132297d01
aiacademy131 <37795049+aiacademy131@users.noreply.github.com>
2021-01-15 01:29:15 +0900

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,9 +1,8 @@
-toolbar = document.getElementById('top-toolbar').style.display = 'none'
-// toolbar = document.getElementById('top-toolbar').style.display = 'flex'
-
-left_panel = document.getElementsByClassName('colab-left-pane-nib')[0].style.display = 'none'
-
-// left_panel = document.getElementsByClassName('colab-left-pane-nib')[0].style.display = 'flex'
+document.getElementById('top-toolbar').style.display = 'none'
+document.getElementsByClassName('top-floater')[0].style.display = 'none'
+// document.getElementById('top-toolbar').style.display = 'flex'
+document.getElementsByClassName('colab-left-pane-nib')[0].style.display = 'none'
+// document.getElementsByClassName('colab-left-pane-nib')[0].style.display = 'flex'
 
 setInterval(()=>{ 
 	main = document.getElementsByClassName('main-content')
