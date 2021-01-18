c41861e942583e2d73b528438a772d323e477ed3
aiacademy131 <37795049+aiacademy131@users.noreply.github.com>
2021-01-18 21:50:02 +0900

    Update and rename customui.js to custom_pro.js

--- custom_pro.js
+++ custom_pro.js
@@ -9,17 +9,17 @@ document.getElementsByClassName('colab-left-pane-nib')[0].style.display = 'none'
 setInterval(()=>{ 
 	main = document.getElementsByClassName('main-content')
 	for (var i = 0; i < main.length; i++) {
-		main[i].style.width = "580px"
+		main[i].style.width = "780px"
 	}
 
 	editor = document.getElementsByClassName('monaco-editor')
 	for (var i = 0; i < editor.length; i++) {
-		editor[i].style.width = "540px"
+		editor[i].style.width = "740px"
 	}
 
 	guard = document.getElementsByClassName('overflow-guard')
 	for (var i = 0; i < guard.length; i++) {
-		guard[i].style.width = "540px"
+		guard[i].style.width = "740px"
 	}
 
 	cell = document.getElementsByClassName('cell-toolbar')
